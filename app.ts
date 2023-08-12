c4078379affbd824f062f22a556d109fc0f12940
Hamza Kamelen <hamzakamelen12345@gmail.com>
2023-08-13 00:21:51 +0500

    Learn TypeScript

---
+++ app.ts
@@ -0,0 +1,76 @@
+//----------------1.Restriction  --------------------------
+
+let a = 5;
+a = 'hamza' //error bcz type already declared through assigning
+a = 6; // Not giving error
+
+let b = 'Hamza';
+b = 6; //error bcz type already declared through assigning
+b = 'HAMZA' // Not giving error
+
+let c = true;
+c = 535 //error bcz type already declared through assigning
+c = false; // Not giving error
+
+let arr = [5,6,4,6]
+arr.push("ABC") //error bcz type already declared through assigning
+arr.push(123); // Not giving error
+
+
+let Arr = ["abc",5,6,4,6]
+Arr.push("ABC"); //Not giving error
+Arr.push(123); // Not giving error
+Arr.push(true); //error bcz type already declared through assigning
+
+
+let ARR = ["abc",true,123]
+ARR.push("Abcccc") // Not giving error
+ARR.push(false) // Not giving error
+ARR.push(123) // Not giving error
+
+
+
+let abc;
+abc = 123; //Not giving error
+abc = true; //Not giving error
+abc = "HAMZA" //Not giving error
+abc = [] //Not giving error
+abc = {}  //Not giving error
+
+
+
+let obj ={
+    name:"HAMZA",
+    id:123
+}
+obj.id = 'HAMZA' //ERROR 
+obj.name = "Hamza" //// Not giving error
+obj.class = "Intermediate" // Doesn't Exist Error bcz we can't add new
+
+//----------------2.Types-----------------------------
+
+let A:string = "ABC";
+A = 123; //error bcz type assigned
+A = "HAMZA" // Not giving error
+
+let B:number = 123;
+B = 'Hamza'; //Error
+B = 123456; // Not giving error
+
+let C:boolean = true;
+C = 'Hamza'; // Error
+C = false; // Not giving error\
+
+let D:any = "AbC"
+D = true; // Not giving error
+D = 123; // Not giving error
+
+let ABCD:number[] = [12,2,3]
+let ABCDE:string[] = ["12","2","A"]
+let ABCDEF:boolean[] = [true,false]
+let ABCDEFG:any[] = ["12","2","A",1,2,3,true]
+let ABCDEFGH:(string || number)[] = ["12","2","A",1,2,3]
+
+let obj1 ={
+    
+}
