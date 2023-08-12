4a1142f49f92083b8f1fe5b23d13775a0d7a5375
Hamza Kamelen <hamzakamelen12345@gmail.com>
2023-08-13 02:56:28 +0500

    Learning..

--- app.ts
+++ app.ts
@@ -69,8 +69,161 @@ let ABCD:number[] = [12,2,3]
 let ABCDE:string[] = ["12","2","A"]
 let ABCDEF:boolean[] = [true,false]
 let ABCDEFG:any[] = ["12","2","A",1,2,3,true]
-let ABCDEFGH:(string || number)[] = ["12","2","A",1,2,3]
+let ABCDEFGH:(string | number)[] = ["12","2","A",1,2,3] // for OR we use | not ||
+// ---------------------------------------------
+let objtype :{
+    name: string,
+    id: number
+}={
+    name:"Hamza",
+    id:122
+}
 
-let obj1 ={
-    
+// ---------------------------------------------
+let objType :{
+    name: string,
+    id: number
+}
+objType ={
+    name:"Hamza",
+    id:122
+}
+
+// ---------------------------------------------
+type ObjecTYpe = {
+    id: number,
+    name: string,
+    isAdmin : boolean
+}
+let user:ObjecTYpe={
+    id:1,
+    name:'Hamza',
+    isAdmin:true
+}
+
+// ---------------------------------------------
+let Types : {
+    name:string,
+    id:number,
+    isAdmin:boolean
+}[]=[
+    {
+        id:123,
+        name:'Hamza',
+        isAdmin:true   
+    }
+]
+
+// ---------------------------------------------
+let usEr:ObjecTYpe[]=[
+    {
+        id:123,
+        name:'Hamza',
+        isAdmin:true
+}
+]
+
+// ---------------------------------------------
+let USER:ObjecTYpe[]=[
+    {
+        id:123,
+        name:'Hamza',
+        isAdmin:true
+},{
+    id:1234,
+    name:'Talha',
+    isAdmin:true
+},{
+    id:1235,
+    name:'Anas',
+    isAdmin:true
+}
+]
+
+// ---------------------------------------------
+type config = {
+    id:number,
+    name:string,
+    theme: "dark" | "light"
+} 
+
+let obj:config = {
+    id:1,
+    name:'abc',
+    theme:"light"
+}
+
+// ---------------------------------------------
+interface User {
+    id:number,
+    name:string,
+    rollNo: string,
+    Contact?:string //Optional
+}
+
+let OBject:User = {
+    id:1,
+    name:"Hamza",
+    rollNo: "22F-BSAI-09"
+}
+
+// ---------------------------------------------
+interface Users {
+    id:number,
+    name:string,
+    rollNo: string,
+    Contact?:string //Optional
+}
+interface Employee extends Users {
+    EmployeeID: number,
+    companyName: string,
+    joiningdate: Date //Date DataType
+}
+
+let object:Employee = {
+    EmployeeID:123456,
+    companyName:'Abc',
+    id : 12,
+    name:'Hamza',
+    rollNo: 'Hamza',
+    joiningdate: new Date();
+}
+
+
+// ---------------------------------------------
+interface Userss <T> {
+    id:number,
+    name:string,
+    rollNo: string,
+    Contact?:string, //Optional
+    extras: T[]
+}
+
+let Emp:Userss <number> ={
+    id:1,
+    name:"Hamza",
+    rollNo: "22F",
+    Contact?:"03112474407", //Optional
+    extras: [1,2,3,4,5,6,7,8]
+}
+
+// ---------------------------------------------
+interface UserSS <T> {
+    id:number,
+    name:string,
+    UserData: T[]
+}
+type user ={
+    Appname:string,
+    Appid:number
+}
+let Data:UserSS <user> ={
+    id:12345,
+    name:"Hamza",
+    UserData: [
+        {
+            Appname:"Hamza",
+            Appid:123 
+        }
+    ]
 }
